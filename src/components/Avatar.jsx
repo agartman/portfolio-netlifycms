42c6de72150ae51788cabdfe160a02e214313f1c
Atte Gartman <atte.gartman@gmail.com>
2017-12-20 09:43:02 +0200

    Some more personalization

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import logo40 from './../images/logo40.jpg';
-import logo24 from './../images/logo24.jpg';
+import React from "react"
+import logo40 from "./../images/logo40.jpg"
+import logo24 from "./../images/logo24.jpg"
 
-const Avatar = (props) => (
+const Avatar = props => (
   <div className={`c-avatar ${props.modifierClasses}`}>
-    <img 
-      src={(props.modifierClasses === 'c-avatar--as-author') ? logo24 : logo40} 
-      alt="greglobinski.com"
+    <img
+      src={props.modifierClasses === "c-avatar--as-author" ? logo24 : logo40}
+      alt="attegartman.com"
     />
   </div>
 )
 
-export default Avatar;
+export default Avatar
